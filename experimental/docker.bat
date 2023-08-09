e8cf6c43ed70d654c94b8d264acc644fc4c5843b
Bjorn Oscarsson <haxorof@users.noreply.github.com>
2023-08-09 13:02:52 +0200

    Deleted experimental directory

--- experimental/docker.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set scriptpath=%~dp0
-del %scriptpath%params.txt
-@REM Input to powershell script is done via file to avoid loosing some special characters.
-for %%x in (%*) do (
-    echo %%~x >> %scriptpath%docker.params
-)
-powershell.exe -Command %scriptpath%docker-helper.ps1
